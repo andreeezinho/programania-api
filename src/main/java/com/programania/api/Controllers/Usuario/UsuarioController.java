4aea1ac1c11365a2b84bafef63a4267823ef2819
andreeezinho <andreeesapucaia@gmail.com>
2026-08-10 20:02:29 -0300

    feat: add methods to basic crud in user controller

--- src/main/java/com/programania/api/Controllers/Usuario/UsuarioController.java
+++ src/main/java/com/programania/api/Controllers/Usuario/UsuarioController.java
@@ -4,17 +4,61 @@ import java.util.UUID;
 
 import com.programania.api.DTO.Usuario.UsuarioPasswordDTO;
 import com.programania.api.Models.Usuario.Usuario;
-//import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.transaction.Transactional;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import com.programania.api.Services.Infra.FileStorageService;
 
 import com.programania.api.Services.Usuario.UsuarioService;
-import org.springframework.web.multipart.MultipartFile;
 
+@RestController
+@RequestMapping("/users")
 public class UsuarioController {
+
+    private UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> index() {
+        return usuarioService.findAll();
+    }
+
+    @GetMapping(path = {"/{uuid}"})
+    public ResponseEntity<?> findByUuid(@PathVariable UUID uuid){
+        return this.usuarioService.findByUuid(uuid);
+    }
+
+    @GetMapping(path = {"/me"})
+    public ResponseEntity<?> userPerfil(@AuthenticationPrincipal UserDetails userDetails){
+        return this.usuarioService.userPerfil(userDetails);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<?> create(@RequestBody Usuario usuario) {
+        return this.usuarioService.create(usuario);
+    }
+
+    @PutMapping(value = "/{uuid}")
+    @Transactional
+    public ResponseEntity<?> update(@PathVariable UUID uuid, @RequestBody Usuario usuario) {
+        return this.usuarioService.update(uuid, usuario);
+    }
+
+    @PutMapping(value = "/{uuid}/password")
+    @Transactional
+    public ResponseEntity<?> updatePassword(@PathVariable UUID uuid, @RequestBody UsuarioPasswordDTO password) {
+        return this.usuarioService.updatePassword(uuid, password);
+    }
+
+    @PutMapping(value = "/{uuid}/delete")
+    @Transactional
+    public ResponseEntity<?> delete(@PathVariable UUID uuid) {
+        return this.usuarioService.delete(uuid);
+    }
+
 }
